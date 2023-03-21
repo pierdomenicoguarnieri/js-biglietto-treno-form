d9c2aa3f67e79a788bbf58a7b9e45779f9d3975c
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-21 15:18:42 +0100

    changed: variable save from prompt to .value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,51 @@
-const km = prompt("Inserisci il numero di chilometri che vuoi percorrere:");
+const generate = document.getElementById("generate");
 
-const age = prompt("Inserisci la tua età:");
+generate.addEventListener("click", function(){
+  
+  const name = document.getElementById("name").value;
 
-let price = (km * 0.21);
+  const km = document.getElementById("km").value;
 
-const output = document.getElementById("content");
+  const age = document.getElementById("ageRange").value;
 
-const outputOffcanvas = document.getElementById("outputOffcanvas");
+  console.log(name, km, age);
 
-if(age < 18){
-  const originalPrice = price;
-  price *= 1 - (20/100);
-  output.innerHTML = `
-  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai inserito la seguente età: ${age} anni.</span> 
-  <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 20% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
-  `
+  let price = (km * 0.21);
 
-  outputOffcanvas.innerHTML = `
-  <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
-  `
-}else if(age >= 65){
-  const originalPrice = price;
-  price *= 1 - (40/100);
-  output.innerHTML = `
-  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai inserito la seguente età: ${age} anni.</span> 
-  <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 40% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
-  `
+  const output = document.getElementById("content");
 
-  outputOffcanvas.innerHTML = `
-  <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
-  `
-}else{
-  output.innerHTML = `
-  <span class="fs-4 py-3">Hai scelto di percorrere ${km}km ed hai inserito la seguente età: ${age} anni.</span> 
-  <p class="fs-5 pb-3">Il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>. Non è stato applicato nessuno sconto.</p>
-  `
+  const outputOffcanvas = document.getElementById("outputOffcanvas");
 
-  outputOffcanvas.innerHTML = `
-  <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
-  `
-}
+  if(age == 2){
+    const originalPrice = price;
+    price *= 1 - (20/100);
+    output.innerHTML = `
+    <span class="fs-4 py-3">Hai scelto di percorrere ${km}km e rientri nella fascia d'età: "Minorenni"</span> 
+    <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 20% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
+    `
+
+    outputOffcanvas.innerHTML = `
+    <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
+    `
+  }else if(age == 3){
+    const originalPrice = price;
+    price *= 1 - (40/100);
+    output.innerHTML = `
+    <span class="fs-4 py-3">Hai scelto di percorrere ${km}km e rientri nella fascia d'età: "Over 65"</span> 
+    <p class="fs-5 pb-3">Il prezzo del biglietto originalmente è di <span class="text-decoration-line-through">${originalPrice.toFixed(2)}€</span>. Dopo aver applicato lo sconto del 40% il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>.</p>
+    `
+
+    outputOffcanvas.innerHTML = `
+    <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
+    `
+  }else{
+    output.innerHTML = `
+    <span class="fs-4 py-3">Hai scelto di percorrere ${km}km e rientri nella fascia d'età: "Maggiorenni"</span> 
+    <p class="fs-5 pb-3">Il prezzo del biglietto è di: <span class="fw-semibold">${price.toFixed(2)}€</span>. Non è stato applicato nessuno sconto.</p>
+    `
+
+    outputOffcanvas.innerHTML = `
+    <span class="py-2">Il totale da pagare è: <span class="fw-semibold">${price.toFixed(2)}€</span>.</span>
+    `
+  }
+})
